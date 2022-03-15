392d2812f31f406ee75515d7fb5ea1f59afaee38
Prime127 <100682813+Prime127@users.noreply.github.com>
2022-03-15 14:14:16 +0300

    Nested Loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+
+while (n < 98)
+{
+	printf("%i, ", n);
+	n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98");
+putchar('\n');
+}
