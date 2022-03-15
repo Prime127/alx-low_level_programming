0b468bca693c6a596d4e3d8199d67bb7a32f00fe
Prime127 <100682813+Prime127@users.noreply.github.com>
2022-03-15 14:16:01 +0300

    Advanced Tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
